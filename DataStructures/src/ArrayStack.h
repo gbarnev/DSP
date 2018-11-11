14ba6c25f5fe5835b5f6ebeb5f7da846c1bbf0fe
Georgi Barnev <Ogbarnev@obecto.com>
2018-11-11 02:11:08 +0200

    Move template implementation in header files of stacks

--- DataStructures/src/ArrayStack.h
+++ DataStructures/src/ArrayStack.h
@@ -9,26 +9,101 @@
 namespace DSP {
 
 	const int MAXSIZE = 200;
-	
+
 	template <typename T = int>
 	class DLLEXPORT ArrayStack
 	{
 	public:
-		ArrayStack(int = MAXSIZE);
-		~ArrayStack();
-		ArrayStack(const ArrayStack<T>&);
-		ArrayStack<T>& operator=(const ArrayStack<T>&);
-		bool empty() const;
-		bool full() const;
-		void push(const T&);
-		void pop();
-		T& top() const;
-		void printDestroy();
+		ArrayStack(int size = MAXSIZE)
+			: stack_size(size), stack_top(-1), arr(nullptr)
+		{
+			if (size <= 0) stack_size = MAXSIZE;
+			arr = new T[stack_size];
+		}
+
+		ArrayStack(const ArrayStack<T>& from)
+		{
+			copyStack(from);
+		}
+
+		virtual ~ArrayStack()
+		{
+			deleteStack();
+		}
+
+		ArrayStack<T>& operator=(const ArrayStack<T>& other)
+		{
+			if (this != &other) {
+				deleteStack();
+				copyStack(other);
+			}
+			return *this;
+		}
+
+		bool empty() const
+		{
+			return stack_top == -1;
+		}
+
+		bool full() const
+		{
+			return stack_top == stack_size - 1;
+		}
+
+		void push(const T& elem)
+		{
+			if (full())
+				throw std::runtime_error("overflow");
+			arr[++stack_top] = elem;
+		}
+
+		void pop()
+		{
+			if (empty())
+				throw std::runtime_error("underflow");
+			stack_top--;
+		}
+
+		T& top() const
+		{
+			if (empty())
+				throw std::runtime_error("underflow");
+			return arr[stack_top];
+		}
+
+		int length() const
+		{
+			return stack_top + 1;
+		}
+
+		void printDestroy()
+		{
+			while (!empty()) {
+				std::cout << top() << " ";
+				pop();
+			}
+			std::cout << std::endl;
+		}
+
 	private:
 		int stack_size;
 		int stack_top;
 		T* arr;
-		void copyStack(const ArrayStack<T>&);
-		void deleteStack();
+
+		void copyStack(const ArrayStack<T>& other)
+		{
+			this->stack_size = other.stack_size;
+			this->stack_top = other.stack_top;
+			this->arr = new T[other.stack_size];
+
+			for (int i = 0; i < stack_size; i++) {
+				arr[i] = other.arr[i];
+			}
+		}
+
+		void deleteStack()
+		{
+			delete[] arr;
+		}
 	};
 }
--- DataStructures/src/LinkedStack.h
+++ DataStructures/src/LinkedStack.h
@@ -12,15 +12,63 @@ namespace DSP {
 	class DLLEXPORT LinkedStack
 	{
 	public:
-		LinkedStack();
-		~LinkedStack();
-		LinkedStack(const LinkedStack<T>&);
-		LinkedStack<T>& operator=(const LinkedStack<T>&);
-		bool empty() const;
-		int length() const;
-		void push(const T&);
-		void pop();
-		T& top() const;
+		LinkedStack()
+			: start(nullptr), len(0) {}
+
+		~LinkedStack()
+		{
+			deleteStack();
+		}
+
+		LinkedStack(const LinkedStack<T>& other)
+		{
+			copyStack(other);
+		}
+
+		LinkedStack<T>& operator=(const LinkedStack<T>& other)
+		{
+			if (this != &other) {
+				deleteStack();
+				copyStack(other);
+			}
+			return *this;
+		}
+
+		bool empty() const
+		{
+			return len == 0;
+		}
+
+		int length() const
+		{
+			return len;
+		}
+
+		void push(const T& elem)
+		{
+			start = new Node{ elem, start };
+			++len;
+		}
+
+		void pop()
+		{
+			if (empty())
+				throw std::out_of_range("underflow");
+
+			Node* temp = start;
+			start = start->link;
+			delete temp;
+			--len;
+		}
+
+		T& top() const
+		{
+			if (empty())
+				throw std::out_of_range("underflow");
+
+			return start->data;
+		}
+
 	private:
 		struct Node
 		{
@@ -29,7 +77,36 @@ namespace DSP {
 		};
 		Node* start;
 		int len;
-		void copyStack(const LinkedStack<T>&);
-		void deleteStack();
+
+		void copyStack(const LinkedStack<T>& other)
+		{
+			this->len = other.len;
+
+			if (other.empty()) {
+				this->start = nullptr;
+				return;
+			}
+
+			this->start = new Node;
+			this->start->data = other.start->data;
+
+			Node* currThis = this->start;
+			Node* tmpOther = other.start->link;
+
+			while (tmpOther != nullptr) {
+				currThis->link = new Node;
+				currThis->link->data = tmpOther->data;
+				currThis = currThis->link;
+				tmpOther = tmpOther->link;
+			}
+			currThis->link = nullptr;
+		}
+
+		void deleteStack()
+		{
+			while (!empty()) {
+				pop();
+			}
+		}
 	};
 }
